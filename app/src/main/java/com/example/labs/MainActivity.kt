9b62b36ae07e26bfe8f438749b1dd4dfd06ed33e
Marco <marcoalsampaio@gmail.com>
2022-03-18 19:34:55 +0000

    LAB FUND-3: Multilingua

--- app/src/main/java/com/example/labs/MainActivity.kt
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.labs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, R.string.hello, Toast.LENGTH_LONG).show()
     }
 }
