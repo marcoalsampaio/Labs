b4e9581bc690ca223c40c9067914906ef13b0407
Marco <marcoalsampaio@gmail.com>
2022-03-18 19:30:42 +0000

    LAB FUND-2: Ciclo de Vida

--- app/src/main/java/com/example/labs/MainActivity.kt
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -7,5 +7,36 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        println("Life cycle: onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        println("Life cycle: onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        println("Life cycle: onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        println("Life cycle: onPause")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        println("Life cycle: onRestart")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        println("Life cycle: onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        println("Life cycle: onDestroy")
     }
 }
