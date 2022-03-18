dc2048a403296b938c189620a9ecc9ddd75bd589
Marco <marcoalsampaio@gmail.com>
2022-03-18 20:11:39 +0000

    LAB FUND-5: Controlos comuns

--- app/src/main/java/com/example/labs/MainActivity.kt
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.labs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun changeName(view: View) {
+        val nomeView = findViewById<TextView>(R.id.NomeView)
+        val nomeEdit = findViewById<EditText>(R.id.NomeEdit)
+        Toast.makeText(this, nomeEdit.text, Toast.LENGTH_LONG).show()
+        nomeView.text = nomeEdit.text
     }
 }
