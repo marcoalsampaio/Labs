a5ca9486c79c847f926cb1fd1f7967097c5f09cc
Marco <marcoalsampaio@gmail.com>
2022-04-21 10:17:07 +0100

    Teste Lab5

--- app/src/main/java/com/example/labs/MainActivity.kt
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.labs
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.CheckBox
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
@@ -11,12 +12,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toast.makeText(this, "Boas Vindas", Toast.LENGTH_LONG).show()
     }
 
-    fun changeName(view: View) {
-        val nomeView = findViewById<TextView>(R.id.NomeView)
-        val nomeEdit = findViewById<EditText>(R.id.NomeEdit)
-        Toast.makeText(this, nomeEdit.text, Toast.LENGTH_LONG).show()
-        nomeView.text = nomeEdit.text
+    override fun onRestart() {
+        super.onRestart()
+        Toast.makeText(this, "Boas Vindas", Toast.LENGTH_LONG).show()
+    }
+
+    fun total(view: View) {
+        val edt1 = findViewById<TextView>(R.id.edt1)
+        val edt2 = findViewById<EditText>(R.id.edt2)
+        val tv = findViewById<TextView>(R.id.tv)
+        val cb = findViewById<CheckBox>(R.id.cb)
+        if (cb.isChecked){
+            var total =  edt1.text.toString().toInt() * edt2.text.toString().toInt() * 1.23;
+            tv.text = total.toString();
+        }else{
+            var total =  edt1.text.toString().toInt() * edt2.text.toString().toInt();
+            tv.text = total.toString();
+        }
+
+
     }
 }
