86f5f0026246e392ebda8c24e32c2ebb6b0078b2
Marco <marcoalsampaio@gmail.com>
2022-05-12 14:16:57 +0100

    inicio

--- app/src/main/java/com/example/labs/MainActivity.kt
+++ app/src/main/java/com/example/labs/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.labs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    fun getSingle(view: View) {}
+    fun post(view: View) {}
 }
