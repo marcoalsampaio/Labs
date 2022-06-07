6eca08a7b773558df61303f7022ab0a320b02552
marcoalsampaio <marcoalsampaio@gmail.com>
2022-06-07 18:39:46 +0100

    LAB Mapa concluido

--- app/src/main/java/com/example/labs/MapsActivity.kt
+++ app/src/main/java/com/example/labs/MapsActivity.kt
@@ -1,8 +1,17 @@
 package com.example.labs
 
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.labs.api.EndPoints
 import com.example.labs.api.ServiceBuilder
 import com.example.labs.api.User
@@ -14,12 +23,20 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.example.labs.databinding.ActivityMapsBinding
-import okhttp3.Response
+import com.google.android.gms.location.*
+import retrofit2.Response
 import retrofit2.Call
 import retrofit2.Callback
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
+    private var continenteLat: Double = 0.0
+    private var continenteLong: Double = 0.0
+    private lateinit var lastLocation: Location
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private val LOCATION_PERMISSION_REQUEST_CODE = 0
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
 
@@ -29,7 +46,35 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        continenteLat = 41.7043
+        continenteLong = -8.8148
+
+
+
+        // request creation
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        createLocationRequest()
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(p0: LocationResult) {
+                super.onLocationResult(p0)
+                lastLocation = p0.lastLocation
+                var loc = LatLng(lastLocation.latitude, lastLocation.longitude)
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(loc, 15.0f))
+                findViewById<TextView>(R.id.txtcoordenadas).setText("Lat: " + loc.latitude + " - Long: " + loc.longitude)
+                Log.d("**** Marco", "new location received - " + loc.latitude + " -" + loc.longitude)
+                val address = getAddress(lastLocation.latitude, lastLocation.longitude)
+                findViewById<TextView>(R.id.txtmorada).setText("Morada: " + address)
+
+                findViewById<TextView>(R.id.txtdistancia).setText("Distância: " + calculateDistance(
+                    lastLocation.latitude, lastLocation.longitude,
+                    continenteLat, continenteLong).toString())
+            }
+        }
+
+        /*val address = getAddress(lastLocation.latitude, lastLocation.longitude)
+        findViewById<TextView>(R.id.txtmorada).setText("Morada: " + address)*/
+
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -48,14 +93,25 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
 
         // Add a marker in Sydney and move the camera
-       /* val sydney = LatLng(-34.0, 151.0)
+        /* val sydney = LatLng(-34.0, 151.0)
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
 
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))*/
         addMarkers(mMap);
+        setUpMap(mMap)
     }
 
-    fun addMarkers(map: GoogleMap){
+    override fun onPause() {
+        super.onPause()
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+        Log.d("**** SARA", "onPause - removeLocationUpdates")
+    }
+    public override fun onResume() {
+        super.onResume()
+        startLocationUpdates()
+        Log.d("**** SARA", "onResume - startLocationUpdates")
+    }
+    fun addMarkers(map: GoogleMap) {
         mMap = map
         var users: List<User>
         val request = ServiceBuilder.buildService(EndPoints::class.java)
@@ -63,15 +119,89 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         var position: LatLng
         call.enqueue(object : Callback<List<User>> {
             override fun onResponse(call: Call<List<User>>, response: Response<List<User>>) {
-                if (response.isSuccessful){
+                if (response.isSuccessful) {
                     users = response.body()!!
                     for (user in users) {
-                        position = LatLng(user.address.geo.lat.toString().toDouble(),user.address.geo.lng.toString().toDouble())
-                        mMap.addMarker(MarkerOptions().position(position).title(user.address.suite + " - " + user.address.city)) } } }
+                        position = LatLng(
+                            user.address.geo.lat.toString().toDouble(),
+                            user.address.geo.lng.toString().toDouble()
+                        )
+                        mMap.addMarker(
+                            MarkerOptions().position(position)
+                                .title(user.address.suite + " - " + user.address.city)
+                        )
+                    }
+                }
+            }
 
             override fun onFailure(call: Call<List<User>>, t: Throwable) {
                 Toast.makeText(this@MapsActivity, "${t.message}", Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    private fun setUpMap(map: GoogleMap) {
+        mMap = map
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE
+            )
+            return
+        } else {
+// 1
+            mMap.isMyLocationEnabled = true
+// 2
+            fusedLocationClient.lastLocation.addOnSuccessListener(this) { location ->// Got last known location. In some rare situations this can be null.
+// 3
+                if (location != null) {
+                    lastLocation = location
+                    Toast.makeText(this@MapsActivity, lastLocation.toString(), Toast.LENGTH_SHORT)
+                        .show()
+                    val currentLatLng = LatLng(location.latitude, location.longitude)
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 12f))
+                }
+            }
+        }
+    }
+
+    private fun createLocationRequest() {
+        locationRequest = LocationRequest()
+// interval specifies the rate at which your app will like to receive updates.
+        locationRequest.interval = 10000
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    }
+
+    private fun startLocationUpdates() {
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
+                LOCATION_PERMISSION_REQUEST_CODE)
+            return
+        }
+        fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null /* Looper */)
+    }
+
+    private fun getAddress(lat: Double, lng: Double): String {
+        val geocoder = Geocoder(this)
+        val list = geocoder.getFromLocation(lat, lng, 1)
+        return list[0].getAddressLine(0)
+    }
+
+    fun calculateDistance(lat1: Double, lng1: Double, lat2: Double, lng2: Double): Float {
+        val results = FloatArray(1)
+        Location.distanceBetween(lat1, lng1, lat2, lng2, results)
+// distance in meter
+        return results[0]
+    }
 }
-}
+
+
+
+
